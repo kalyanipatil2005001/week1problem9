22141995d664b3ba125f36916ea092a1c16d2882
family <family@MILIND>
2020-12-09 14:05:18 +0530

    structure of model created

---
+++ w1p9.m
@@ -0,0 +1,22 @@
+%Initialize the values for constant parameters in the model
+
+%Poles of motor
+P = 4;
+
+%Stator phase resistance
+R = 4.98;
+
+%Stator Phase inductance
+L = 5.05e-3;
+
+% Inertia 
+J = 15.17e-6;
+
+%Friction Factor
+B = 1e-6;
+
+%Back-EMF coefficient
+Ke = 56.23e-6;
+ 
+%Load Torque
+Tm = 0.1;
